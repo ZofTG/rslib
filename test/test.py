92637df58f4dda3e61987510553d3f4b90bb6679
ZofTG <lzoffoli@technogym.com>
2023-06-02 16:23:34 +0200

    refactoring

--- test/test.py
+++ test/test.py
@@ -4,6 +4,6 @@ import os
 import pylab
 
 if __name__ == "__main__":
-    tdf_file = os.path.sep.join([os.getcwd(), "test", "tdf_test.tdf"])
-    tdf_data = pylab.read_tdf(tdf_file)
+    TDF_FILE = os.path.sep.join([os.getcwd(), "test", "tdf_test_file.tdf"])
+    tdf_data = pylab.read_tdf(TDF_FILE)
     check = 1
