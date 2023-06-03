99f8eb006c38b7810aaf2156a6672376f2cffb6f
ZofTG <lzoffoli@technogym.com>
2023-06-03 12:21:39 +0200

    added pip install specs

---
+++ tests/test.py
@@ -0,0 +1,12 @@
+"""pylab testing module"""
+
+import os
+import sys
+
+sys.path += [os.getcwd()]
+import rslib
+
+if __name__ == "__main__":
+    TDF_FILE = os.path.sep.join([os.getcwd(), "test", "tdf_sample.tdf"])
+    tdf_data = rslib.read_tdf(TDF_FILE)
+    check = 1
